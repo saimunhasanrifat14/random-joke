cc2dd09c62d49608944dd56b012ce3ac8c682aa5
Saimun Hasan <saimunhasanrifat14@gmail.com>
2025-02-04 02:05:39 +0600

    complate the project

--- js/main.js
+++ js/main.js
@@ -1,18 +1,28 @@
 const joke = document.querySelector(".joke");
 const btn = document.querySelector(".btn");
+let listen = document.querySelector(".listen");
+let randomJoke;
+let speech = new SpeechSynthesisUtterance();
+
 const url =
   "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
-let getJoke = ()=>{
-    joke.classList.remove("fade")
-    fetch(url)
-    .then(data => data.json())
-    .then(item => {
-        joke.textContent = `${item.joke}`
-        joke.classList.add("fade")
-    }
-)
-}
-btn.addEventListener("click", ()=>{
-    getJoke()
 
-})
+let getJoke = () => {
+  joke.classList.remove("fade");
+  fetch(url)
+    .then((data) => data.json())
+    .then((item) => {
+      randomJoke = joke.textContent = `${item.joke}`;
+      joke.classList.add("fade");
+      speech.text = randomJoke;
+    });
+};
+listen.addEventListener("click", () => {
+  if (randomJoke) {
+    speech.text = randomJoke;
+    window.speechSynthesis.speak(speech);
+  }
+});
+btn.addEventListener("click", () => {
+  getJoke();
+});
