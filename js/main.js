7adf60e8f8435fe20e8c803362636e5d80c1e45c
Saimun Hasan <saimunhasanrifat14@gmail.com>
2025-02-04 00:54:40 +0600

    add js code

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const joke = document.querySelector(".joke");
+const btn = document.querySelector(".btn");
+const url =
+  "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
+let getJoke = ()=>{
+    joke.classList.remove("fade")
+    fetch(url)
+    .then(data => data.json())
+    .then(item => {
+        joke.textContent = `${item.joke}`
+        joke.classList.add("fade")
+    }
+)
+}
+btn.addEventListener("click", ()=>{
+    getJoke()
+
+})
